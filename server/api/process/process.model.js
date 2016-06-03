3101a225babbd93b0295a5a5d96bbb0b458fab53
pradeet <pradeet1995@gmail.com>
2016-06-04 01:10:44 +0530

    Added Process Model

--- server/api/process/process.model.js
+++ server/api/process/process.model.js
@@ -3,9 +3,28 @@
 import mongoose from 'mongoose';
 
 var ProcessSchema = new mongoose.Schema({
-  name: String,
-  info: String,
-  active: Boolean
+  processId: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'ProcessDetails',
+    required: true
+  },
+  studentId: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  selected: {
+    type: Boolean,
+    default: false
+  },
+  isPresent: {
+    type: Boolean,
+    default: false
+  },
+  timeStamp: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 export default mongoose.model('Process', ProcessSchema);
